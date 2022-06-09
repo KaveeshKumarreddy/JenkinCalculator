62c26ec449e179df95778899dc38a2a84047f7d7
KaveeshKumarreddy <kaveeshkumarreddy@gmail.com>
2022-06-09 22:35:44 +0530

    modified the main method to excecute the calculations

--- src/main/java/com/github/KaveeshKumarreddy/JenkinCalculator/App.java
+++ src/main/java/com/github/KaveeshKumarreddy/JenkinCalculator/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Calculator cal = new Calculator();
         System.out.println( "Hello World!" );
+        System.out.println("adding 10 and 20 will give :"+cal.add(10, 20));
+        System.out.println("subtracting 20 and 10 will give :"+cal.sub(20, 10));
     }
 }
